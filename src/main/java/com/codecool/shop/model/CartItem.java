6da3ba293d4578dbb6b3a5c9f06e7a516568ba53
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-26 13:58:22 +0300

    Implement all attributes for CartItem object.

--- src/main/java/com/codecool/shop/model/CartItem.java
+++ src/main/java/com/codecool/shop/model/CartItem.java
@@ -1,4 +1,9 @@
 package com.codecool.shop.model;
 
 public class CartItem {
+    private int id;
+    private String name;
+    private int piece;
+    private int sumItemPrice;
+    private int itemPrice;
 }
