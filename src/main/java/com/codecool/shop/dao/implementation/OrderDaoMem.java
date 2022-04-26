a73260c0620c0cc8bf8206674c335884a45ccbbc
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-26 14:08:51 +0300

    Generate all setters for OrderDaoMem.

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -55,4 +55,36 @@ public class OrderDaoMem {
     public int getTotalPrice() {
         return totalPrice;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public void setPaymentAccepted(boolean paymentAccepted) {
+        this.paymentAccepted = paymentAccepted;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
