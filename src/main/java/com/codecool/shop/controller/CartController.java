306452af69a7ffe9ff6aa8ca33e7ced80dff0e74
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-26 14:14:20 +0300

    Create overriding method doGet.

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -2,10 +2,20 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.implementation.OrderDaoMem;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/cart"})
 public class CartController extends HttpServlet {
-    OrderDaoMem orderDaoMem = OrderDaoMem.getInstance();
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        OrderDaoMem orderDaoMem = OrderDaoMem.getInstance();
+        super.doGet(req, resp);
+    }
 }
