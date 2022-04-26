97f3dd2961e0ba19d6d2e13452bbb7bf8bc5cf30
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-26 14:04:13 +0300

    Create Singleton, getInstance in OrderDaoMem.

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -1,6 +1,9 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.model.CartItem;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class OrderDaoMem {
     private String name;
@@ -9,7 +12,15 @@ public class OrderDaoMem {
     private String billingAddress;
     private String shippingAddress;
     private boolean paymentSucceeded;
-    private List<cartItem> cartItems = new ArrayList<>();
+    private List<CartItem> cartItems = new ArrayList<>();
     private static OrderDaoMem instance = null;
     private int totalPrice;
+
+    private OrderDaoMem() {
+    }
+
+    public static OrderDaoMem getInstance() {
+        if (instance == null) instance = new OrderDaoMem();
+        return instance;
+    }
 }
