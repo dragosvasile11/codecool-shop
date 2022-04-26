e7f383c366f8d75a9fd61fb6b3e38557fe473fe0
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-26 14:11:22 +0300

    Instantiate new OrderDaoMem object.

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,7 +1,11 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.implementation.OrderDaoMem;
+
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
 @WebServlet(urlPatterns = {"/cart"})
-public class CartController {
+public class CartController extends HttpServlet {
+    OrderDaoMem orderDaoMem = OrderDaoMem.getInstance();
 }
