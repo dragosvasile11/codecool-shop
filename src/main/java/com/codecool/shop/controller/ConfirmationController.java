ec8b5e1f3b02c63b0b665d1a1646d259573a51ae
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-26 16:34:30 +0300

    Create ConfirmationController Servlet.

---
+++ src/main/java/com/codecool/shop/controller/ConfirmationController.java
@@ -0,0 +1,9 @@
+package com.codecool.shop.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/confirmation"})
+public class ConfirmationController extends HttpServlet {
+
+}
