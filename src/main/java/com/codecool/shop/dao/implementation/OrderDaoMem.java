8c0c964621b6269d5df76fe86f0e75d07621bd04
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-26 13:53:20 +0300

    Implement all attributes for OrderDaoMem object.

--- src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -1,4 +1,15 @@
 package com.codecool.shop.dao.implementation;
 
+import java.util.ArrayList;
+
 public class OrderDaoMem {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String billingAddress;
+    private String shippingAddress;
+    private boolean paymentSucceeded;
+    private List<cartItem> cartItems = new ArrayList<>();
+    private static OrderDaoMem instance = null;
+    private int totalPrice;
 }
