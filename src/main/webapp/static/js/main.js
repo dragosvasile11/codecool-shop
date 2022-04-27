69aa68c6aed5c8420ae44c37fdf346e295c106dd
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-27 14:37:12 +0300

    Add event listener for checkoutButton.

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -3,3 +3,9 @@ document.getElementById("cartButton").addEventListener("click", openCart);
 function openCart() {
     window.location.href = "http://localhost:8888/view-cart";
 }
+
+document.getElementById("checkoutButton").addEventListener("click", openCheckoutPage);
+
+function openCheckoutPage() {
+
+}
