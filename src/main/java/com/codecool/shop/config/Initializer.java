48eecaafda37795fe54bc4edd6b088dadaa62ea3
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-04-28 19:34:02 +0300

    Add more suppliers and products

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -29,14 +29,27 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
+        Supplier google = new Supplier("Google", "Mobile devices");
+        supplierDataStore.add(google);
+        Supplier samsung = new Supplier("Samsung", "Mobile devices");
+        supplierDataStore.add(samsung);
+        Supplier hp = new Supplier("HP", "Computers");
+        supplierDataStore.add(hp);
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory phone = new ProductCategory("Phone", "Hardware", "A mobile computer that fits in your pocket.");
+        productCategoryDataStore.add(phone);
+        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "Best computer to write code on");
+        productCategoryDataStore.add(laptop);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Google Pixel", new BigDecimal("299"), "USD", "Google's best mobile phone.", phone, google));
+        productDataStore.add(new Product("Samsung S22", new BigDecimal("499"), "USD", "With samsung you have the power in your pocket.", phone, samsung));
+        productDataStore.add(new Product("HP Envy", new BigDecimal("899"), "USD", "Luxury laptop for people who chose form over functionality.", laptop, hp));
     }
 }
