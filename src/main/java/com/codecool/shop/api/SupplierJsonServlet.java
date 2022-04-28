4dc25b784ed5a6d973e6f70c5deee8667e441d68
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-28 18:31:19 +0300

    Create session api servlet for supplier.

---
+++ src/main/java/com/codecool/shop/api/SupplierJsonServlet.java
@@ -0,0 +1,36 @@
+package com.codecool.shop.api;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.service.ProductService;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "SupplierJsonServlet", urlPatterns = "/api/supplier")
+public class SupplierJsonServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        Gson gson = new Gson();
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDao = SupplierDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore, supplierDao);
+        String supplierId = request.getParameter("supplier");
+        out.println(gson.toJson(productService.getProductsForSupplier(Integer.parseInt(supplierId))));
+    }
+}
