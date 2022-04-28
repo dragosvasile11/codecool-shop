0dc04ccd4f3bb153226a1a85e2d11de72dd1b946
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-28 21:51:50 +0300

    Add async handling for confirmation html.

--- src/main/webapp/static/js/handleConfirmationPage.js
+++ src/main/webapp/static/js/handleConfirmationPage.js
@@ -1,4 +1,26 @@
-
+getCart().then(data => {
+    let idP = document.getElementById("order-id");
+    idP.textContent = "Order ID: " + localStorage.getItem("orderId");
+    let detailsDiv = document.querySelector("#product-list");
+    let totalSum = 0;
+    let prodNumber = 0;
+    let newProdList = "";
+    for (let item of data) {
+        console.log(item)
+        newProdList += item['amount'] + " " + item['name'] + "(s), ";
+        prodNumber++;
+    }
+    detailsDiv.textContent = newProdList.substring(0, newProdList.length - 2);
+    for (let item of data) {
+        totalSum += parseFloat(item['defaultPrice']) * parseFloat(item['amount']);
+    }
+    console.log(totalSum)
+    let sum = document.createElement('p');
+    document.querySelector("#item-number").textContent = prodNumber.toString() + " items";
+    document.querySelector(".subtotal").textContent = "$" + totalSum.toFixed(2).toString();
+    document.querySelector(".total-price").textContent = "$" + (totalSum + 1.80).toFixed(2).toString();
+    detailsDiv.appendChild(sum);
+}).then(clearCart);
 
 async function clearCart() {
     await fetch("/api/session/removeAll", {method: 'GET',})
