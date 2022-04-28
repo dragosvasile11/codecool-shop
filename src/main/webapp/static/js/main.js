6727b9223eb49ed56ae9a87b714f8b854e90dc42
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-28 10:12:18 +0300

    Number of items near cart icon is increased while add to cart.

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -6,6 +6,19 @@ function openCart() {
 
 document.getElementById("checkoutButton").addEventListener("click", openCheckoutPage);
 
+
 function openCheckoutPage() {
 
+}
+
+const els = document.getElementsByClassName("btn btn-success");
+console.log(els);
+
+for (let el of els) {
+    el.addEventListener("click", addItem);
+    function addItem() {
+        let cartNumberOfItems = document.getElementById("itemsNumberInCart").innerHTML;
+        let numberAsInteger = parseInt(cartNumberOfItems.slice(1, cartNumberOfItems.length - 1));
+        document.getElementById("itemsNumberInCart").innerHTML = "(" + (numberAsInteger + 1) + ")";
+    }
 }
