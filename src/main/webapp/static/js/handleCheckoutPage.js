82f48bc46d7d2843e1b8e2b0077618635d24d564
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-28 21:55:19 +0300

    Add fieldLog function.

--- src/main/webapp/static/js/handleCheckoutPage.js
+++ src/main/webapp/static/js/handleCheckoutPage.js
@@ -25,4 +25,18 @@ function fieldHandler(e) {
     logOutput.push(`${e.target.id} input occurred`);
 }
 
+function fieldLog() {
+    logOutput.push("FIELD STATUSES ON CHECKOUT EVENT")
+    for (let field of requiredFields) {
+        if (field.id === "email") {
+            logOutput.push(field.id + ": " + field.value)
+        } else {
+            let validInput = field.value.length > 0 ? "yes" : "no"
+            let fieldStatus = field.id + " valid input: " + validInput;
+            logOutput.push(fieldStatus);
+        }
+    }
+}
+
+
 
