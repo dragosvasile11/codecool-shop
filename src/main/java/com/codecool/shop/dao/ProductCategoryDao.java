730a40518125c2242ded9573cd9ec35b26fdf263
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-04-29 07:08:28 +0300

    Add methods to interface

--- src/main/java/com/codecool/shop/dao/ProductCategoryDao.java
+++ src/main/java/com/codecool/shop/dao/ProductCategoryDao.java
@@ -3,10 +3,13 @@ package com.codecool.shop.dao;
 import com.codecool.shop.model.ProductCategory;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ProductCategoryDao {
 
     void add(ProductCategory category);
+    List<String> getNames();
+    Map<Integer, String> getIdsAndNames();
     ProductCategory find(int id);
     void remove(int id);
 
